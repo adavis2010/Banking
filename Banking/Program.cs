08fee9e5fdf23e265bcadde28f0f1fed924ea308
max-student <max-student@maxtrain.com>
2021-02-15 16:28:17 -0500

    Add project files.

---
+++ Banking/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Banking {
+    class Program {
+        static void Main(string[] args) {
+
+            var acct1 = new Account();
+            acct1.AccountNumber = "100";
+            Console.WriteLine ($"Account {acct1.AccountNumber} balance is {acct1. Balance}");
+
+            acct1.Deposit(200);
+            Console.WriteLine($"Account {acct1.AccountNumber} balance is {acct1.Balance}");
+
+            acct1.Withdrawal(30);
+            Console.WriteLine($"Account {acct1.AccountNumber} balance is {acct1.Balance}");
+
+            acct1.Withdrawal(230);
+            Console.WriteLine($"Account {acct1.AccountNumber} balance is {acct1.Balance}");
+
+            acct1.Deposit(-200);
+            acct1.Withdrawal(-200);
+            Console.WriteLine($"Account {acct1.AccountNumber} balance is {acct1.Balance}");
+
+        }
+    }
+}
