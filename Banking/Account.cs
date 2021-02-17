f33ba17ceff6df814d3c78a5084cfa6d0833456b
max-student <max-student@maxtrain.com>
2021-02-17 13:35:05 -0500

    add

--- Banking/Account.cs
+++ Banking/Account.cs
@@ -4,11 +4,9 @@ using System.Text;
 
 namespace Banking {
     class Account {
-        public string AccountNumber { get; set; }
-        public string RoutingNumber { get; set; }
+        public string AccountNumber { get; private set; }
         public string Description { get; set; }
-        public string AccountType { get; set; }
-        public decimal Balance { get; set; } = 0;
+        public decimal Balance { get; private set; } = 0;
 
         public void Deposit(decimal amount) {
             if (amount >= 0) {
@@ -16,7 +14,7 @@ namespace Banking {
             }
         }
         public void Withdrawal(decimal amount) {
-            if (amount < 0 ){
+            if (amount < 0) {
                 return;
             }
             if (Balance >= amount) {
@@ -26,12 +24,29 @@ namespace Banking {
                 Console.WriteLine($"Insufficient Funds!");
 
             }
-            
-
-
-        } 
 
 
 
+        }
+
+        public Account(): this("DefAcct0", "Default Account Description") { }
+
+
+        public Account(string acctNbr, string desc = "New Account") {
+            AccountNumber = acctNbr;
+            Description = desc;
+        }
     }
 }
+            
+            
+            
+                
+
+
+        
+
+
+
+    
+
--- Banking/Program.cs
+++ Banking/Program.cs
@@ -4,8 +4,8 @@ namespace Banking {
     class Program {
         static void Main(string[] args) {
 
-            var acct1 = new Account();
-            acct1.AccountNumber = "100";
+            var acct1 = new Account("Acct101", "Andrea's Account");
+
             Console.WriteLine ($"Account {acct1.AccountNumber} balance is {acct1. Balance}");
 
             acct1.Deposit(200);
